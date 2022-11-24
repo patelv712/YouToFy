4b1d831409ea208734687be1de136f53916f2393
Varun Patel <varunpatel@varuns-mbp-2.lan>
2022-11-24 12:56:53 -0500

    add user authentication

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import useAuth from './useAuth'
 
 function Dashboard({code}) {
+    const access_token = useAuth(code)
   return (
     <div> {code} </div>
   )
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const SpotifyWebAPI = require('spotify-web-api-node');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/login", (req, res) => {
     const code = req.body.code
