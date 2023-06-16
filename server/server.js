55a6a31f436c6e8042b4bde8e074c34aa23b33a8
Varun Patel <varunpatel@varuns-mbp-2.lan>
2023-06-16 10:51:37 -0400

    400 error fix

--- server/server.js
+++ server/server.js
@@ -1,31 +1,56 @@
-const express = require('express');
-const app = express();
-const SpotifyWebApi = require('spotify-web-api-node');
-const cors = require('cors');
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const SpotifyWebApi = require("spotify-web-api-node")
 
+const app = express()
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: 'b60f9fa389e64c0a89774d66fd2dc6f3',
+    clientSecret: '054df06b187041058a022a520d858fc3',
+    refreshToken,
+  })
 
-app.post("/login", (req, res) => {
-    const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-      redirectUri: 'http://localhost:3000',
-      clientId: 'b60f9fa389e64c0a89774d66fd2dc6f3',
-      clientSecret: '054df06b187041058a022a520d858fc3',
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
     })
-
-
-    spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({
-            accessToken: data.body.access_token,
-            refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in
-        })
-    }).catch(() => {
-        res.sendStatus(400)
+    .catch(err => {
+        onsole.log(refreshToken)
+      console.log(err)
+      res.sendStatus(500)
     })
 })
 
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: 'b60f9fa389e64c0a89774d66fd2dc6f3',
+    clientSecret: '054df06b187041058a022a520d858fc3',
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      res.sendStatus(400)
+    })
+})
 app.listen(3001)
-
-
